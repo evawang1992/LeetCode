feca171982489d3fd57933a2ef07680eb8c8a172
XiaochenWang <kaka9682215@gmail.com>
2022-02-22 19:18:15 -0800

    Time: 121 ms (76.10%), Space: 43.8 MB (42.86%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.java
+++ 212-word-search-ii/212-word-search-ii.java
@@ -15,12 +15,6 @@ class Solution {
                 dfs(board, root, result, i, j);
             }
         }
-        // for(int i = 0; i < board.length; i++){
-        //     for(int j = 0; j < board[0].length; j++){
-        //         System.out.print(board[i][j]);
-        //     }
-        //     System.out.println();
-        // }
         return result;
     }
     void dfs(char[][] board, Trie root, List<String> result, int x, int y){
