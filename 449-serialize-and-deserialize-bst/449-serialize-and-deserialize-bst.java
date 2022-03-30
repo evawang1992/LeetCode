86c367535217d6a2b24086f7efaadc9bc3a93c92
XiaochenWang <kaka9682215@gmail.com>
2022-03-30 10:53:02 -0700

    Time: 17 ms (37.40%), Space: 49.3 MB (32.45%) - LeetHub

--- 449-serialize-and-deserialize-bst/449-serialize-and-deserialize-bst.java
+++ 449-serialize-and-deserialize-bst/449-serialize-and-deserialize-bst.java
@@ -11,28 +11,35 @@ public class Codec {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        if(root == null) return ",";
-        return root.val+","+serialize(root.left)+serialize(root.right);
+        // if(root == null) return "";
+        StringBuilder sb = new StringBuilder();
+        serialize(root, sb);
+        return sb.toString();
+    }
+    public void serialize(TreeNode root, StringBuilder sb){
+        if(root == null) return;
+        sb.append(root.val+",");
+        serialize(root.left, sb);
+        serialize(root.right, sb);
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        String[] arr = data.split(",");
-        Queue<String> q = new LinkedList<>();
-        q.addAll(Arrays.asList(arr));
-        return deserializeTree(q);
-        
+        if(data == null || data.length() == 0)return null;
+        String[] sarr = data.split(",");
+        Queue<String> q = new LinkedList<String>(Arrays.asList(sarr));
+        return deserialize(q, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
     
-    TreeNode deserializeTree(Queue<String> q){
-            if(q.isEmpty()) return null;
-            String s = q.poll();
-        if(s.isEmpty()) return null;
-        TreeNode root = new TreeNode(Integer.parseInt(s));
-        root.left = deserializeTree(q);
-        root.right = deserializeTree(q);
-        return root;
-        
+    TreeNode deserialize(Queue<String> q, int lower, int upper){
+        if(q.isEmpty()) return null;
+        int num = Integer.valueOf(q.peek());
+        if(num < lower || num > upper) return null;
+        q.poll();
+        TreeNode newNode = new TreeNode(num);
+        newNode.left = deserialize(q, lower, num);
+        newNode.right = deserialize(q, num, upper);
+        return newNode;
     }
 }
 
