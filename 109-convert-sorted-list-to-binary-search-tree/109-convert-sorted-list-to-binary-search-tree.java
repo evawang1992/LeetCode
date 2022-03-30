ddc9c070517d8c6963b7859386c9af76d8fe1fb6
XiaochenWang <kaka9682215@gmail.com>
2022-03-30 10:05:35 -0700

    Time: 1 ms (86.27%), Space: 47.4 MB (33.73%) - LeetHub

--- 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.java
+++ 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.java
@@ -23,28 +23,23 @@
  *     }
  * }
  */
-class Solution {
-    ListNode node;
-    public TreeNode sortedListToBST(ListNode head) {
-        if(head == null) return null;
-        ListNode h = head;
-        int size = 0;
-        node = head;
-        while(h != null){
-            h = h.next;
-            size++;
-        }
-        return sort(0, size-1);
+public class Solution {
+public TreeNode sortedListToBST(ListNode head) {
+    if(head==null) return null;
+    return toBST(head,null);
+}
+public TreeNode toBST(ListNode head, ListNode tail){
+    ListNode slow = head;
+    ListNode fast = head;
+    if(head==tail) return null;
+    
+    while(fast!=tail&&fast.next!=tail){
+        fast = fast.next.next;
+        slow = slow.next;
     }
-    TreeNode sort(int start, int end){
-        if(start > end) return null;
-        int mid = (start+end)/2;
-        TreeNode left = sort(start,mid-1);
-        TreeNode newNode = new TreeNode(node.val);
-        newNode.left = left;
-        node = node.next;
-        TreeNode right = sort(mid+1, end);
-        newNode.right = right;
-        return newNode;
-    }
+    TreeNode thead = new TreeNode(slow.val);
+    thead.left = toBST(head,slow);
+    thead.right = toBST(slow.next,tail);
+    return thead;
+}
 }
